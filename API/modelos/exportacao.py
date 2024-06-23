35f2fa46fcfaeb2fe4a340e0587236b689c7cc5b
Osman Lira Junior <osman.lira@gmail.com>
2024-06-23 11:32:19 -0300

    entidade exportacao

--- API/modelos/exportacao.py
+++ API/modelos/exportacao.py
@@ -1,5 +1,17 @@
 class Exportacao:
-    def __init__(self, pais, quantidade,tipo_uva):
+    """
+    Classe que representa um registro de exposrtacao de vinho, uvas, espumantes e suco.
+
+    Atributos:
+        pais (str): Nome do pais.
+        quantidade (int): Quantidade exposrtada.
+        valor (float): valor exportado
+        tipo_exportacao (str): Tipo do produto (e.g., VINHO DE MESA, ESPUMANTES, UVAS FRESCAS, SUCO DE UVA).
+        ano (int): Ano de exportacao.
+    """
+    def __init__(self, pais,quantidade, valor,tipo_exportacao,ano):
         self._nome = pais
         self._quantidade = quantidade
-        self._tipo_uva = tipo_uva
+        self._valor = valor
+        self._tipo_exportacao = tipo_exportacao
+        self._ano = ano
