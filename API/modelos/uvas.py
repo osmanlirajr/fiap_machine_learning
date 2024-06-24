70bc8266c1f4299e86685a61780e2eef404ca08f
Letícia <leticiapyres@gmail.com>
2024-06-24 16:07:56 -0400

    Update uvas.py

--- API/modelos/uvas.py
+++ API/modelos/uvas.py
@@ -1,6 +1,7 @@
 class Uvas:
-    def __init__(self, nome, quantidade,tipo, classificacao):
+    def __init__(self, nome, quantidade,tipo, classificacao, ano):
         self._nome = nome
         self._quantidade = quantidade
         self._tipo = tipo
         self._classificacao = classificacao
+        self._ano = ano
